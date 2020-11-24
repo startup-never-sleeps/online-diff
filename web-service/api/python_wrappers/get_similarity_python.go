0c6e70e01a15a97f715ed07d1f947c3e4bbd2b0f
Artyom Gevorgyan <43173333+gevorgyana@users.noreply.github.com>
2020-11-24 14:39:23 +0200

    Accept JSON string output from Python similarity script. (#52)
    Signed-off-by: Artyom Gevorgyan <artemhevorhian@gmail.com>

--- web-service/api/python_wrappers/get_similarity_python.go
+++ web-service/api/python_wrappers/get_similarity_python.go
@@ -6,7 +6,6 @@ import (
 	"os/exec"
 	"path"
 	"path/filepath"
-	"strconv"
 	"strings"
 )
 
@@ -15,11 +14,11 @@ const (
 	python_interpreter_path = "/usr/bin/python3"
 )
 
-func GetPairwiseSimilarity(input_path string, args ...string) ([][]float32, error) {
+func GetPairwiseSimilarity(input_path string, args ...string) (string, error) {
 	var err error
 	var cur_path string
 	if cur_path, err = filepath.Abs(filepath.Dir(os.Args[0])); err != nil {
-		return nil, err
+		return "", err
 	}
 
 	var execute_path = python_script_path
@@ -39,21 +38,8 @@ func GetPairwiseSimilarity(input_path string, args ...string) ([][]float32, erro
 	cmd.Stderr = os.Stderr
 
 	if err = cmd.Run(); err != nil {
-		return nil, err
+		return "", err
 	}
 
-	out_arr := strings.Split(pipe_out.String(), ",")
-	mat_size, _ := strconv.Atoi(out_arr[0])
-
-	res_mat := make([][]float32, mat_size)
-	for i := range res_mat {
-		res_mat[i] = make([]float32, mat_size)
-	}
-
-	for idx, upper_b := 1, mat_size*mat_size; idx < upper_b; idx++ {
-		val, _ := strconv.ParseFloat(out_arr[idx], 32)
-		res_mat[idx/mat_size][idx%mat_size] = float32(val)
-	}
-
-	return res_mat, nil
+	return pipe_out.String(), nil
 }
